79a940cfddcbef399d552c3998e52088762c5c0f
Mathias2800 <mathias@valling.net>
2014-12-18 20:36:47 +0100

    done done

--- LoanBankThree/src/loanbankthree/Bank.java
+++ LoanBankThree/src/loanbankthree/Bank.java
@@ -16,17 +16,19 @@ public class Bank {
 
     public Float getInterestRate(int creditScore, int loanDuration, double amount){
         float credit = creditScore;
-        float result = BASE_RATE + ((800 - credit)/100);
+        float result = BASE_RATE + ((801 - credit)/100);
         if(amount < 10000){
             result += 0.3f;
         }else if(amount < 100000){
             result += 0.18f;
-        }
+        }else
+            result += 0.6f;
         if(loanDuration < 180){
             result += 3.0f;
         }else if(loanDuration < 720){
             result += 1.4f;
-        }
+        }else
+            result += 4.0f;
         return result;
     }
     
